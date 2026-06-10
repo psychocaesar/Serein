1ee40abe8da72cda6957d050a27b2f6a4886421e
César Broche Aguilar <contact@cesarbroche.fr>
2026-06-10 20:23:51 +0200

    MAJ HOMEPAGE

--- psygest-pwa/src/public/js/sw-init.js
+++
@@ -1,6 +0,0 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
-      .catch(err => console.warn('SW registration failed:', err));
-  });
-}
